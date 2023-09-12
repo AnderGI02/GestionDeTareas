6cf385c9cae9bb9020f7655547984d9798d6ab93
AnderGI <ander.gutierrez.ibarguen@zabalburu.org>
2023-09-12 11:52:39 +0200

    Creado projecto

---
+++ GestionDeTareas/src/main/java/org/zabalburu/GestionDeTareas/jakarta/hello/Hello.java
@@ -0,0 +1,14 @@
+package org.zabalburu.GestionDeTareas.jakarta.hello;
+
+public class Hello {
+
+	private String name;
+	
+	public Hello(String name) {
+        this.name = name;
+	}
+
+	public String getHello(){
+		return name;
+	}
+}
